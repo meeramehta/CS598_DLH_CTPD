b4f9ac1242f21c500f25d0a5933de23d56c307dd
root <root@ip-172-31-75-60.ec2.internal>
2025-12-07 04:36:40 +0000

    add ablations cript

--- scripts/mimic3/train_ctpd_ihm.sh
+++ scripts/mimic3/train_ctpd_ihm.sh
@@ -1,4 +1,4 @@
-python train_mimic3.py --task ihm --model_name ctpd --devices 1 --accelerator cpu \
-   --pooling_type attention \
-   --use_prototype \
-   --use_multiscale --lamb1 0.1 --lamb2 0.5 --lamb3 0.5
+ CUDA_VISIBLE_DEVICES=1 python train_mimic3.py --task ihm --model_name ctpd --devices 1 \
+    --pooling_type attention \
+    --use_prototype \
+    --use_multiscale --lamb1 0.1 --lamb2 0.5 --lamb3 0.5 
